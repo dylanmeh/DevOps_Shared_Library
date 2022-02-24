5769d01a59835d695215146b7f122c5a85cd7e07
dylanmeh <dylan.mehmedovic@concanon.com>
2022-02-24 10:20:25 -0500

    changes shared lib body syntax

--- vars/buildAndDeployDockerfiles.groovy
+++ vars/buildAndDeployDockerfiles.groovy
@@ -1,17 +1,13 @@
 def call() {
-    stage('build docker images') {
-        container('alpine-docker') {
-            git credentialsId: 'GitHub_Creds' url: 'https://github.com/dylanmeh/DevOps_Dockerfiles.git'
-            sh '''
-            cd Dockerfiles/Maven
-            docker build -t dylanmehmedovic/alpine-docker .
-            echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u $DOCKERHUB_CREDENTIALS_USR --password-stdin
-            docker push dylanmehmedovic/alpine-docker
-            cd ../Gradle
-            docker build -t dylanmehmedovic/alpine-docker .
-            echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u $DOCKERHUB_CREDENTIALS_USR --password-stdin
-            docker push dylanmehmedovic/alpine-docker
-            '''
-        }    
-    }
+    git credentialsId: 'GitHub_Creds' url: 'https://github.com/dylanmeh/DevOps_Dockerfiles.git'
+    sh '''
+    cd Dockerfiles/Maven
+    docker build -t dylanmehmedovic/alpine-docker .
+    echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u $DOCKERHUB_CREDENTIALS_USR --password-stdin
+    docker push dylanmehmedovic/alpine-docker
+    cd ../Gradle
+    docker build -t dylanmehmedovic/alpine-docker .
+    echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u $DOCKERHUB_CREDENTIALS_USR --password-stdin
+    docker push dylanmehmedovic/alpine-docker
+    '''   
 }    
