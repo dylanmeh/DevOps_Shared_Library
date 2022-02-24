4184da7af4b0633722269a93d385ab94217013d3
dylanmeh <dylan.mehmedovic@concanon.com>
2022-02-24 15:28:36 -0500

    ..

--- vars/buildAndDeployDockerfiles.groovy
+++ vars/buildAndDeployDockerfiles.groovy
@@ -3,8 +3,7 @@ def call() {
     container('alpine-docker') {
         stage('build and push docker images') {
             sh '''
-            pwd 
-            ls
+            cd .. | ls 
             cd Dockerfiles/Maven
             docker build -t dylanmehmedovic/alpine-docker .
             echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u $DOCKERHUB_CREDENTIALS_USR --password-stdin
