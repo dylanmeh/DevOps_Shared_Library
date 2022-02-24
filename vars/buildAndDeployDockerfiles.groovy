b6b665d540ab49803d420978f866eede086e290a
dylanmeh <dylan.mehmedovic@concanon.com>
2022-02-24 15:41:09 -0500

    ..

--- vars/buildAndDeployDockerfiles.groovy
+++ vars/buildAndDeployDockerfiles.groovy
@@ -1,19 +1,25 @@
 def call() {
-    git 'https://github.com/dylanmeh/DevOps_Dockerfiles.git'
-    container('alpine-docker') {
-        stage('build and push docker images') {
-            sh '''
-            pwd
-            ls 
-            cd Dockerfiles/Maven
-            docker build -t dylanmehmedovic/alpine-docker .
-            echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u $DOCKERHUB_CREDENTIALS_USR --password-stdin
-            docker push dylanmehmedovic/alpine-docker
-            cd ../Gradle
-            docker build -t dylanmehmedovic/alpine-docker .
-            echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u $DOCKERHUB_CREDENTIALS_USR --password-stdin
-            docker push dylanmehmedovic/alpine-docker
-            '''
-        }    
-    }       
+    stage('build and push docker images') {
+        steps {
+            script {
+                container('alpine-docker') {
+                    git 'https://github.com/dylanmeh/DevOps_Dockerfiles.git'
+                    stage('build and push docker images') { 
+                        sh '''
+                        pwd
+                        ls 
+                        cd Dockerfiles/Maven
+                        docker build -t dylanmehmedovic/alpine-docker .
+                        echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u $DOCKERHUB_CREDENTIALS_USR --password-stdin
+                        docker push dylanmehmedovic/alpine-docker
+                        cd ../Gradle
+                        docker build -t dylanmehmedovic/alpine-docker .
+                        echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u $DOCKERHUB_CREDENTIALS_USR --password-stdin
+                        docker push dylanmehmedovic/alpine-docker
+                        '''
+                    }
+                }
+            }
+        }                
+    }          
 }    
